93c3b152f08686aaf9a3e27bc6be2921f2b263be
Vinicius <vinicius.marcili@gmail.com>
2025-11-17 20:44:45 -0300

    Add temperature conversion functions and agent initialization

---
+++ src/1.3.own_tools.py
@@ -0,0 +1,40 @@
+from agno.agent import Agent
+from agno.tools.tavily import TavilyTools
+from agno.models.groq import Groq
+from dotenv import load_dotenv
+load_dotenv()
+
+def celsius_to_fahrenheit(celsius: float) -> float:
+  """
+    Converts Celsius to Fahrenheit.
+    
+    Args:
+      celsius (float): Temperature in Celsius.
+      
+    Returns:
+      float: Temperature in Fahrenheit.
+  """
+  return (celsius * 9/5) + 32
+
+def fahrenheit_to_celsius(fahrenheit: float) -> float:
+  """
+    Converts Fahrenheit to Celsius.
+    
+    Args:
+      fahrenheit (float): Temperature in Fahrenheit.
+    
+    Returns:
+      float: Temperature in Celsius.
+  """
+  return (fahrenheit - 32) * 5/9
+
+
+model = Groq(id='llama-3.3-70b-versatile')
+
+agent = Agent(
+  tools=[TavilyTools(), celsius_to_fahrenheit, fahrenheit_to_celsius],
+  model=model,
+  debug_mode=True
+)
+
+agent.print_response("Use sua ferramentas para pesquisar sobre temperatura em São Paulo amanhã. Mostre em Celsius e Fahrenheit.", stream=True)
